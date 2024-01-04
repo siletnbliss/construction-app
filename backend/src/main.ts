d118480a3fff3627d4fcc58ae8afe61561241737
Luis Alejandro Castillo Martínez <luiscasm2501@gmail.com>
2024-01-03 21:50:44 -0400

    Base docker configuration of api

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = parseInt(process.env.SERVER_PORT || '3000');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
