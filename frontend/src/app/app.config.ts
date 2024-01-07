fc5d83b0d1fe4ccb6b05b49dfab280eeeec6d0a4
Luis Alejandro Castillo Martínez <luiscasm2501@gmail.com>
2024-01-06 23:09:14 -0400

    FRONTEND: taiga UI installation

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { TuiRootModule } from "@taiga-ui/core";
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideAnimations(), provideRouter(routes), provideClientHydration(), importProvidersFrom(TuiRootModule)]
 };
