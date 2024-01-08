f3f1cd2b599c4e18f23a3b3abad3f2b5bdb2496d
Luis Alejandro Castillo Martínez <luiscasm2501@gmail.com>
2024-01-08 00:58:49 -0400

    BACKEND: enabled cors to allow all origins

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const port = parseInt(process.env.SERVER_PORT || '3000');
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: true });
   await app.listen(port);
 }
 bootstrap();
