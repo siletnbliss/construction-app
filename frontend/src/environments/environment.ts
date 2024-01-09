e70d405c7a384e01e5d0fa28fb4ad11891732b61
Luis Alejandro Castillo Martínez <luiscasm2501@gmail.com>
2024-01-09 07:33:26 -0400

    FROTEND: production preparations

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // TODO: set production url
-  apiUrl: '',
+  apiUrl: 'https://constructiona-api.onrender.com',
   // to persist auth in session storage
   sessionStorageAuthKey: 'E+28s_cqSUK6:;[u',
 };
